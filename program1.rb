ccb6fdc91ead5e60318c0ca8a9969846f5c0c6fb
kadenhargrove <103163582+kadenhargrove@users.noreply.github.com>
2022-04-26 16:32:54 -0500

    Add files via upload

---
+++ program1.rb
@@ -0,0 +1,7 @@
+#Program 1
+#One line if statement - prints number if it is even
+#even? is the boolean method that returns true or false
+#The ? is Ruby code convention for naming boolean methods
+number = 8
+puts number if number.even?
+puts
---
+++ program3.rb
@@ -0,0 +1,15 @@
+#Program 3
+#Range loop, times loop, each loop
+
+#each loop
+list1 = [1,2,3,4,5]
+list1.each {|n| puts n}
+puts
+
+#range looping
+(6..10).each {|n| puts n}
+puts
+
+#times loop
+num = 10
+5.times {puts num = num + 1}
